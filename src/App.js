f865d7b6b26260fa88421740a30c1a0146e45bd8
javi <jsmart1993@hotmail.com>
2020-06-11 07:30:09 -0700

    gave a background minor css changes to header

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function App() {
       </ThoughtListContextProvider>
       </BrowserRouter>
       
-       <BackToTop/>
+       
     </main>
     </div>
   );
--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT:process.env.REACT_APP_API_ENDPOINT || 'https://secret-lowlands-06468.herokuapp.com',
+  API_ENDPOINT:process.env.REACT_APP_API_ENDPOINT || 'https://secret-lowlands-06468.herokuapp.com/api',
   API_KEY: process.env.REACT_APP_API_KEY,
 }
